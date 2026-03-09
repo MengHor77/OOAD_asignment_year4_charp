f31695f876c53bc49e1832a8a53a85c62a143f1d
meng hor <piseth125125@gmail.com>
2026-03-09 11:38:49 +0700

    add form product

---
+++ POS_Inventory/Form/AdminForm/Page/Product/ProductForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_Inventory.Form.AdminForm.Page.Product
+{
+    public partial class ProductForm : Form
+    {
+        public ProductForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
