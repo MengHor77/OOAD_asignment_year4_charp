8c8fd35aa48c1afa9dc0d67f5ac30c5c25bcd798
meng hor <piseth125125@gmail.com>
2026-03-11 14:30:47 +0700

    fix: style

--- POS_Inventory/Form/POSForm/ItemsOrder.cs
+++ POS_Inventory/Form/POSForm/ItemsOrder.cs
@@ -45,11 +45,151 @@ namespace POS_Inventory.Form.POSForm
                 control.Region = new Region(path);
             }
         }
+        //private void SetupOrderRowDesign()
+        //{
+        //    // --- UserControl properties ---
+        //    this.Size = new Size(330, 60);
+        //    this.BackColor = Color.LightGray; // temporary background
+        //    this.Margin = new Padding(0, 8, 0, 8);
+
+        //    int centerY = this.Height / 2;
+
+        //    // --- Apply rounded corners to UserControl ---
+        //    this.Load += (s, e) => ApplyRounding(this, 15);
+        //    this.Resize += (s, e) => ApplyRounding(this, 15);
+
+        //    // --- Item Name Label ---
+        //    lblItemName = new Label
+        //    {
+        //        Text = "iphone",
+        //        ForeColor = Color.Black,
+        //        Font = new Font("Segoe UI", 12, FontStyle.Regular),
+        //        AutoSize = false,
+        //        Size = new Size(150, 25),
+        //        TextAlign = ContentAlignment.MiddleLeft,
+        //        Location = new Point(10, centerY - 20)
+        //    };
+
+        //    // --- Price Per Unit Label ---
+        //    lblPricePerUnit = new Label
+        //    {
+        //        Text = "30$/ unit",
+        //        ForeColor = Color.Black,
+        //        Font = new Font("Segoe UI", 9, FontStyle.Regular),
+        //        AutoSize = false,
+        //        Size = new Size(80, 20),
+        //        TextAlign = ContentAlignment.MiddleLeft,
+        //        Location = new Point(10, centerY + 8)
+        //    };
+
+        //    int circleSize = 30; // Width = Height for perfect circle
+        //    int circleRadius = circleSize / 2;
+
+        //    // --- Plus Button (circular) ---
+        //    btnPlus = new Label
+        //    {
+        //        Text = "+",
+        //        ForeColor = Color.White,
+        //        BackColor = AppColorConfig.CardPOSProduct,
+        //        Font = new Font("Segoe UI", 10, FontStyle.Bold),
+        //        TextAlign = ContentAlignment.MiddleCenter,
+        //        Size = new Size(circleSize, circleSize),
+        //        Cursor = Cursors.Hand,
+        //        Location = new Point(180, centerY - circleRadius)
+        //    };
+        //    btnPlus.MouseEnter += (s, e) => btnPlus.BackColor = AppColorConfig.LightBlue;
+        //    btnPlus.MouseLeave += (s, e) => btnPlus.BackColor = AppColorConfig.CardPOSProduct;
+        //    btnPlus.Click += (s, e) =>
+        //    {
+        //        int qty = int.Parse(lblQty.Text);
+        //        qty++;
+        //        lblQty.Text = qty.ToString();
+        //        new ItemOrderConfig().UpdateQty(ItemName, qty);
+        //        OnQuantityChanged?.Invoke();
+        //    };
+        //    ApplyRounding(btnPlus, circleRadius); // perfect circle
+
+        //    // --- Quantity Label ---
+        //    lblQty = new Label
+        //    {
+        //        Text = "3",
+        //        ForeColor = Color.Black,
+        //        Font = new Font("Segoe UI", 10, FontStyle.Bold),
+        //        AutoSize = false,
+        //        Size = new Size(20, circleSize),
+        //        TextAlign = ContentAlignment.MiddleCenter,
+        //        Location = new Point(220, centerY - circleRadius)
+        //    };
+
+        //    // --- Minus Button (circular) ---
+        //    btnMinus = new Label
+        //    {
+        //        Text = "-",
+        //        ForeColor = Color.White,
+        //        BackColor = AppColorConfig.CardPOSProduct,
+        //        Font = new Font("Segoe UI", 10, FontStyle.Bold),
+        //        TextAlign = ContentAlignment.MiddleCenter,
+        //        Size = new Size(circleSize, circleSize),
+        //        Cursor = Cursors.Hand,
+        //        Location = new Point(250, centerY - circleRadius)
+        //    };
+        //    btnMinus.MouseEnter += (s, e) => btnMinus.BackColor = AppColorConfig.LightBlue ;
+        //    btnMinus.MouseLeave += (s, e) => btnMinus.BackColor = AppColorConfig.CardPOSProduct; 
+        //    btnMinus.Click += (s, e) =>
+        //    {
+        //        int qty = int.Parse(lblQty.Text);
+        //        if (qty > 1)
+        //        {
+        //            qty--;
+        //            lblQty.Text = qty.ToString();
+        //            new ItemOrderConfig().UpdateQty(ItemName, qty);
+        //            OnQuantityChanged?.Invoke();
+        //        }
+        //    };
+        //    ApplyRounding(btnMinus, circleRadius); // perfect circle
+
+        //    // --- Delete Button (X circular) ---
+        //    btnDelete = new Label
+        //    {
+        //        Size = new Size(circleSize, circleSize),
+        //        BackColor = AppColorConfig.CardPOSProduct,
+        //        ForeColor = Color.White,
+        //        Cursor = Cursors.Hand,
+        //        Location = new Point(290, centerY - circleRadius)
+        //    };
+        //    btnDelete.MouseEnter += (s, e) => btnDelete.BackColor = AppColorConfig.LightBlue;
+        //    btnDelete.MouseLeave += (s, e) => btnDelete.BackColor = AppColorConfig.CardPOSProduct;
+
+        //    btnDelete.Paint += (s, e) =>
+        //    {
+        //        using (var b = new SolidBrush(Color.White))
+        //        {
+        //            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        //            e.Graphics.DrawString("X", new Font("Segoe UI", 10, FontStyle.Bold), b, new PointF(7, 5));
+        //        }
+        //    };
+        //    btnDelete.Click += (s, e) =>
+        //    {
+        //        new ItemOrderConfig().DeleteItem(ItemName);
+        //        this.Parent.Controls.Remove(this);
+        //        OnItemDeleted?.Invoke();
+        //    };
+        //    ApplyRounding(btnDelete, circleRadius); // perfect circle
+
+        //    // --- Add controls ---
+        //    this.Controls.Add(lblItemName);
+        //    this.Controls.Add(lblPricePerUnit);
+        //    this.Controls.Add(btnPlus);
+        //    this.Controls.Add(lblQty);
+        //    this.Controls.Add(btnMinus);
+        //    this.Controls.Add(btnDelete);
+        //}
+
         private void SetupOrderRowDesign()
         {
             // --- UserControl properties ---
             this.Size = new Size(330, 60);
-            this.BackColor = Color.LightGray; // temporary background
+            this.BackColor = Color.LightGray;
             this.Margin = new Padding(0, 8, 0, 8);
 
             int centerY = this.Height / 2;
@@ -82,7 +222,7 @@ namespace POS_Inventory.Form.POSForm
                 Location = new Point(10, centerY + 8)
             };
 
-            int circleSize = 30; // Width = Height for perfect circle
+            int circleSize = 30;
             int circleRadius = circleSize / 2;
 
             // --- Plus Button (circular) ---
@@ -107,12 +247,19 @@ namespace POS_Inventory.Form.POSForm
                 new ItemOrderConfig().UpdateQty(ItemName, qty);
                 OnQuantityChanged?.Invoke();
             };
-            ApplyRounding(btnPlus, circleRadius); // perfect circle
+            btnPlus.Paint += (s, e) =>
+            {
+                using (System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath())
+                {
+                    gp.AddEllipse(0, 0, btnPlus.Width - 1, btnPlus.Height - 1);
+                    btnPlus.Region = new Region(gp);
+                }
+            };
 
             // --- Quantity Label ---
             lblQty = new Label
             {
-                Text = "3",
+                Text = "1",
                 ForeColor = Color.Black,
                 Font = new Font("Segoe UI", 10, FontStyle.Bold),
                 AutoSize = false,
@@ -133,8 +280,8 @@ namespace POS_Inventory.Form.POSForm
                 Cursor = Cursors.Hand,
                 Location = new Point(250, centerY - circleRadius)
             };
-            btnMinus.MouseEnter += (s, e) => btnMinus.BackColor = AppColorConfig.LightBlue ;
-            btnMinus.MouseLeave += (s, e) => btnMinus.BackColor = AppColorConfig.CardPOSProduct; 
+            btnMinus.MouseEnter += (s, e) => btnMinus.BackColor = AppColorConfig.LightBlue;
+            btnMinus.MouseLeave += (s, e) => btnMinus.BackColor = AppColorConfig.CardPOSProduct;
             btnMinus.Click += (s, e) =>
             {
                 int qty = int.Parse(lblQty.Text);
@@ -146,7 +293,14 @@ namespace POS_Inventory.Form.POSForm
                     OnQuantityChanged?.Invoke();
                 }
             };
-            ApplyRounding(btnMinus, circleRadius); // perfect circle
+            btnMinus.Paint += (s, e) =>
+            {
+                using (System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath())
+                {
+                    gp.AddEllipse(0, 0, btnMinus.Width - 1, btnMinus.Height - 1);
+                    btnMinus.Region = new Region(gp);
+                }
+            };
 
             // --- Delete Button (X circular) ---
             btnDelete = new Label
@@ -159,13 +313,17 @@ namespace POS_Inventory.Form.POSForm
             };
             btnDelete.MouseEnter += (s, e) => btnDelete.BackColor = AppColorConfig.LightBlue;
             btnDelete.MouseLeave += (s, e) => btnDelete.BackColor = AppColorConfig.CardPOSProduct;
-
             btnDelete.Paint += (s, e) =>
             {
                 using (var b = new SolidBrush(Color.White))
                 {
                     e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
                     e.Graphics.DrawString("X", new Font("Segoe UI", 10, FontStyle.Bold), b, new PointF(7, 5));
+                }
+                using (System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath())
+                {
+                    gp.AddEllipse(0, 0, btnDelete.Width - 1, btnDelete.Height - 1);
+                    btnDelete.Region = new Region(gp);
                 }
             };
             btnDelete.Click += (s, e) =>
@@ -174,7 +332,6 @@ namespace POS_Inventory.Form.POSForm
                 this.Parent.Controls.Remove(this);
                 OnItemDeleted?.Invoke();
             };
-            ApplyRounding(btnDelete, circleRadius); // perfect circle
 
             // --- Add controls ---
             this.Controls.Add(lblItemName);
